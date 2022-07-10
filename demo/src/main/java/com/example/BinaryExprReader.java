c534d0fe8451510cf34a23b91f951cc0b2f19376
RickCompileError <gngn15401@gmail.com>
2022-07-10 18:48:02 +0800

    Add Binary Expression Reader

---
+++ demo/src/main/java/com/example/BinaryExprReader.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.List;
+
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.Expression;
+
+public class BinaryExprReader{
+
+    public static void read(BinaryExpr be, NodeList<Expression> nl, List<BinaryExpr.Operator> ls){
+        if (be.getLeft() instanceof BinaryExpr) BinaryExprReader.read((BinaryExpr)be.getLeft(), nl, ls);
+        else nl.add(be.getLeft());
+        nl.add(be.getRight());
+        ls.add(be.getOperator());
+    }
+
+}
