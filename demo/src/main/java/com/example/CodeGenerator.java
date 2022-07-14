0c29a6313e1273f9669ed163026233854d8850a5
RickCompileError <gngn15401@gmail.com>
2022-07-14 22:11:42 +0800

    Rename Class name

--- demo/src/main/java/com/example/CodeGenerator.java
+++ demo/src/main/java/com/example/CodeGenerator.java
@@ -8,29 +8,42 @@ import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSol
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 
 import java.io.*;
-import java.io.FileInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.regex.Matcher;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class SQLParser {
+public class CodeGenerator {
 
     private static final String Base_Path = System.getProperty("user.dir") + "/demo/src/main/webapp/";
     
-    public static void main(String[] args) throws Exception {
-        Path path = getJSON();
-        System.out.println("Get JSON File: " + path);
+    public static void main(String[] args) {
+        CodeGenerator cg = new CodeGenerator();
+        try{
+            if (args.length>0){
+                for (String i: args){
+                    cg.process(Path.of(i));
+                }
+            }else{
+                cg.process();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void process(Path path) throws Exception{
         SnykVulnerable sv = JsonReader.getSnykVulnerable(path);
 
         setParser();
 
-        removePrevFile();
         for (Vulnerable v: sv.get("java/Sqli")){
+            removePrevFile(v.getFilePath());
             System.out.println("Start solving\n\t"+v);
             CompilationUnit cu = StaticJavaParser.parse(new FileInputStream(Base_Path + v.getFilePath()));
             LexicalPreservingPrinter.setup(cu);
@@ -44,44 +57,54 @@ public class SQLParser {
             System.out.println("Done");
         }
     }
+
+    public void process() throws Exception{
+        List<Path> path = getJSON();
+        System.out.println("Get JSON File: " + path);
+        for (Path i: path) process(i);
+    }
     
     /*
      * Get scanning json file by JFileChooser
      */
-    public static Path getJSON(){
+    public List<Path> getJSON(){
         JFileChooser chooser = new JFileChooser();
         chooser.setCurrentDirectory(new File(Base_Path));
         chooser.setFileFilter(new FileNameExtensionFilter("JSON File", "json"));
+        chooser.setMultiSelectionEnabled(true);
         int returnValue = chooser.showOpenDialog(null);
         if (returnValue==JFileChooser.APPROVE_OPTION){
-            return chooser.getSelectedFile().toPath();
+            List<File> file = Arrays.asList(chooser.getSelectedFiles());
+            List<Path> path = new ArrayList<>();
+            for (File i: file) path.add(i.toPath());
+            return path;
         }
         return null;
     }
 
-    public static void setParser() {
+    public void setParser() {
         StaticJavaParser.getConfiguration().setAttributeComments(true);
         JavaSymbolSolver jss = new JavaSymbolSolver(new CombinedTypeSolver(new ReflectionTypeSolver()));
         StaticJavaParser.getConfiguration().setSymbolResolver(jss);
     }
 
-    public static void removePrevFile(){
+    public void removePrevFile(String file){
         try{
             for (Path path: Files.newDirectoryStream(Path.of(Base_Path))){
-                if (Pattern.matches("n\\d+_.*", path.getFileName().toString())) Files.delete(path);
+                if (Pattern.matches("n\\d+_"+file, path.getFileName().toString())) Files.delete(path);
             }
         }catch (IOException ioe){
             ioe.printStackTrace();
         }
     }
 
-    public static int getVersionNumber(String file_name){
+    public int getVersionNumber(String file_name){
         int v = 1;
         while (Files.exists(Path.of(Base_Path + "n" + String.valueOf(v) + "_" + file_name))) v++;
         return v;
     }
 
-    public static void Save(String file_name, String output){
+    public void Save(String file_name, String output){
         Path path = Path.of(Base_Path+file_name);
         try{
             Files.writeString(path, output);
