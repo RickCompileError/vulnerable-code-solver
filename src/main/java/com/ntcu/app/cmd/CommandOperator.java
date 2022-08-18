e655fb19215ad525b02fa0d26e2f8dd300538e5e
RickCompileError <gngn15401@gmail.com>
2022-08-18 13:54:59 +0800

    add new functions

--- src/main/java/com/ntcu/app/cmd/CommandOperator.java
+++ src/main/java/com/ntcu/app/cmd/CommandOperator.java
@@ -5,26 +5,40 @@ import java.io.InputStreamReader;
 
 public class CommandOperator{
 
+    // For Linux 
     public static void diff(String old_dir, String new_dir){   
-        System.out.println("Start comparing\n");        
         String cmd = "diff " + old_dir + " " + new_dir;
-        try {
-            // Run command
+        System.out.println("old: " + old_dir + "\n" + "new: " + new_dir + "\n");
+        System.out.println("Result:\n");
+        exec(cmd);
+    } 
+
+    // For Windows
+    public static void dir(){
+        String cmd = "cmd /c dir";
+        exec(cmd);
+    }
+
+    // For Windows
+    public static void cd(){
+        String cmd = "cmd /c cd";
+        exec(cmd);
+    }
+
+    private static void exec(String cmd){
+        try{
             Process process = Runtime.getRuntime().exec(cmd);
 
-            // Get input streams
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
             // Read command standard output
             String s;
-            System.out.println("old: " + old_dir + "\n" + "new: " + new_dir + "\n");
             System.out.println("Result:\n");
             while ((s = stdInput.readLine()) != null) System.out.println(s);
-            System.out.println("---------------------------------------------------------------------------");
         } 
         catch (Exception e) {
             e.printStackTrace(System.err);
         }  
-    } 
+    }
 
 }
