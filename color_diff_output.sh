fefcd7884ab9687ae9ed469ac6a266eb053c4b4a
Dennis Huang <dennis0116@gmail.com>
2022-11-26 08:24:22 +0000

    Update color_diff_output.sh

--- color_diff_output.sh
+++ color_diff_output.sh
@@ -5,40 +5,40 @@ counter=0
 #While loop to read line by line
 while IFS= read -r line; do
     if [[ $counter -lt 2 ]] ; then
-        printf "\e[36m$line\e[0m\n"
+        printf "\033[36m$line\033[0m\n"
         counter=$((counter+1))
         continue
     fi
     if [[ $counter -ge 2  &&  $line == \*\*\*\** ]] ; then
-        printf "aaaaaaa\e[30m$line\e[0m\n"
+        printf "\033[30m$line\033[0m\n"
         continue
     fi
     if [[ $counter -ge 2  &&  $line == \*\*\*\ * ]] ; then
-        printf "bbbbbbb\e[35m$line\e[0m\n"
+        printf "\033[35m$line\033[0m\n"
         color="red"
         continue
     fi
     if [[ $counter -ge 2  &&  $line == ---\ * ]] ; then
-        printf "ccccccc\e[35m$line\e[0m\n"
+        printf "\033[35m$line\033[0m\n"
         color="green"
         continue
     fi
     if [[ $counter -ge 2  &&  $color == "red" ]] ; then
         if [[ $line == !\ * ]] ; then
-            printf "dddddd\e[106m$line\e[0m\n"
+            printf "\033[106m$line\033[0m\n"
         elif [[ $line == -\ * ]] ; then
-            printf "eeeeeee\e[101m$line\e[0m\n"
+            printf "\033[101m$line\033[0m\n"
         else
-            printf "fffffff\e[91m$line\e[0m\n"
+            printf "\033[91m$line\033[0m\n"
         fi
     fi
     if [[ $counter -ge 2  &&  $color == "green" ]] ; then
         if [[ $line == !\ * ]] ; then
-            printf "ggggggg\e[106m$line\e[0m\n"
+            printf "\033[106m$line\033[0m\n"
         elif [[ $line == +\ * ]] ; then
-            printf "hhhhhhh\e[102m$line\e[0m\n"
+            printf "\033[102m$line\033[0m\n"
         else
-            printf "iiiiiii\e[32m$line\e[0m\n"
+            printf "\033[32m$line\033[0m\n"
         fi
     fi
 
