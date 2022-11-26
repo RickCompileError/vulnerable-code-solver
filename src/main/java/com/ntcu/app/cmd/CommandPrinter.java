b5c2bdb502e36a12f3bcf0821aa1cab11d7cf7fa
Dennis Huang <dennis0116@gmail.com>
2022-11-26 16:02:26 +0000

    Update src/main/java/com/ntcu/app/cmd/CommandPrinter.java

--- src/main/java/com/ntcu/app/cmd/CommandPrinter.java
+++ src/main/java/com/ntcu/app/cmd/CommandPrinter.java
@@ -39,6 +39,61 @@ public class CommandPrinter{
         }
     }
 
+    public static void colorPrint(){
+        String file_name = "diff.txt";
+        File f = new File(file_name);
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(f));
+            String line = null;
+            String color = null;
+            System.out.println("\\e[36m"+reader.readLine()+"\\e[0m");
+            System.out.println("\\e[36m"+reader.readLine()+"\\e[0m");
+            while (true){
+                line = reader.readLine();
+                if (line==null) break;
+                if (line.matches("\\*\\*\\*\\*.*")){
+                    System.out.println("\\e[30m"+line+"\\e[0m");
+                    continue;
+                }
+                if (line.matches("\\*\\*\\* .*")){
+                    System.out.println("\\e[35m"+line+"\\e[0m");
+                    color = "red";
+                    continue;
+                }
+                if (line.matches("--- .*")){
+                    System.out.println("\\e[35m"+line+"\\e[0m");
+                    color = "green";
+                    continue;
+                }
+                if (color=="red"){
+                    if (line.matches("! .*")){
+                        System.out.println("\\e[106m"+line+"\\e[0m");
+                    }
+                    else if (line.matches("- .*")){
+                        System.out.println("\\e[101m"+line+"\\e[0m");
+                    }
+                    else{
+                        System.out.println("\\e[91m"+line+"\\e[0m");
+                    }
+                }
+                if (color=="green"){
+                    if (line.matches("! .*")){
+                        System.out.println("\\e[106m"+line+"\\e[0m");
+                    }
+                    else if (line.matches("- .*")){
+                        System.out.println("\\e[102m"+line+"\\e[0m");
+                    }
+                    else{
+                        System.out.println("\\e[32m"+line+"\\e[0m");
+                    }
+                }
+            }
+            reader.close();
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+
     public static void createSnykSH(){
         try{
             File f = new File("snyk.sh");
